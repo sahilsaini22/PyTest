c4183c97a91698a5762718ab89f51f22ea446809
nathanielcarolina <nathanielcarolina@gmail.com>
2020-06-08 08:31:52 +0200

    Update python load file

--- test.py
+++ test.py
@@ -1,10 +1,58 @@
-from pymongo import MongoClient
-import pandas
-import json
+
+from py2neo import Graph
+ 
+def loadneonodes():
+    graph = Graph(host='localhost', port=7687, password="root")
+#Clean up
+    graph.delete_all()
+ 
+ 
+#Read the csv file and create the song nodes
+    query = """
+    LOAD CSV WITH HEADERS FROM 'file:///neoData.csv' AS column 
+    MERGE (s:Songs{name:column.Song,release:toInteger(column.Year),genre:column.Genre,album:column.Album,duration:column.Duration})
+ 
+    """
+    graph.run(query)
+
+#Read the csv file and create the artist nodes
+    query = """
+    LOAD CSV WITH HEADERS FROM 'file:///neoData.csv' AS column MERGE (a:Artists {a_name:column.Artist})
+ 
+    """
+    graph.run(query)
 
 
-try: 
-    conn = MongoClient() 
-    print("Connected successfully!!!") 
-except:   
-    print("Could not connect to MongoDB") 
+    
+#Read the csv file and create the genre nodes
+    query = """
+    LOAD CSV WITH HEADERS FROM 'file:///neoData.csv' AS column MERGE (g:Genres {genre:column.Genre})
+ 
+    """
+    graph.run(query)
+
+#relation between the artists and the songs
+    query = """
+    LOAD CSV WITH HEADERS FROM 'file:///neoData.csv' AS column MERGE (s:Songs{name:column.Song,release:toInteger(column.Year),genre:column.Genre,album:column.Album,duration:column.Duration})
+    MERGE (a:Artists {a_name:column.Artist})
+    MERGE (s)-[:sungBy]->(a)
+ 
+    """
+    graph.run(query)
+
+
+    
+
+#relation between the genre and the songs
+    query = """
+    LOAD CSV WITH HEADERS FROM 'file:///neoData.csv' AS column MERGE (s:Songs{name:column.Song,release:toInteger(column.Year),genre:column.Genre,album:column.Album,duration:column.Duration})
+    MERGE (g:Genres {genre:column.Genre})
+    MERGE (s)-[:belongsTo]->(g)
+ 
+    """
+    graph.run(query)
+ 
+
+# start 
+if __name__ == '__main__':
+    loadneonodes()
